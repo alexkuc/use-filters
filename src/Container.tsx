7d6cade15bc3231eaa4082e07f0c3bb9f712ccd2
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2022-06-12 22:01:16 +0300

    Add time-based filters

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,9 +1,23 @@
 import { action } from 'mobx';
-import { observer } from 'mobx-react-lite';
-import { includes, isEmpty } from 'lodash';
-import { useEffect } from 'react';
 import { Store } from './Store';
-import { FilterArray, FilterScalar } from './Filters';
+import { useEffect } from 'react';
+import { includes, isEmpty } from 'lodash';
+import { observer } from 'mobx-react-lite';
+import { FilterByArr, FilterByMap, FilterByVal } from './Filters';
+
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import isToday from 'dayjs/plugin/isToday';
+import isTomorrow from 'dayjs/plugin/isTomorrow';
+import weekOfYear from 'dayjs/plugin/weekOfYear';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(isToday);
+dayjs.extend(isTomorrow);
+dayjs.extend(weekOfYear);
+
 import './style.scss';
 import 'normalize.css';
 
@@ -33,7 +47,43 @@ const Container = observer((props: ContainerProps) => {
         },
       })
     );
+    props.store.addFilter(
+      new FilterByMap({
+        key: 'time',
+        apply: function (item): boolean {
+          if (this.value?.size === 0) return true;
+          const arr = this.value?.toArray();
+          return arr!.some((f) => f.apply(item) === true);
+        },
+      })
+    );
   }, []);
+
+  const isToday = new FilterByVal({
+    key: 'isToday',
+    apply: (item) => dayjs(item.starTime).isToday(),
+  });
+
+  const isTomorrow = new FilterByVal({
+    key: 'isTomorrow',
+    apply: (item) => dayjs(item.starTime).isTomorrow(),
+  });
+
+  const isThisWeek = new FilterByVal({
+    key: 'isThisWeek',
+    apply: (item) => dayjs(item.starTime).week() === dayjs().week(),
+  });
+
+  const isThisMonth = new FilterByVal({
+    key: 'isThisMonth',
+    apply: (item) => dayjs(item.starTime).month() === dayjs().month(),
+  });
+
+  const isNextMonth = new FilterByVal({
+    key: 'isNextMonth',
+    apply: (item) =>
+      dayjs(item.starTime).month() === dayjs().add(1, 'month').month(),
+  });
 
   return (
     <div className="container">
@@ -75,6 +125,77 @@ const Container = observer((props: ContainerProps) => {
             />
           </label>
         ))}
+      <br />
+      <label>
+        today
+        <input
+          type="checkbox"
+          checked={props.store.getFilter('time')?.has(isToday) ?? false}
+          onChange={action((e) => {
+            const c = e.currentTarget.checked;
+            const f = props.store.getFilter('time');
+
+            if (c) f.add(isToday);
+            if (!c) f.remove(isToday);
+          })}
+        />
+      </label>
+      <label>
+        tomorrow
+        <input
+          type="checkbox"
+          checked={props.store.getFilter('time')?.has(isTomorrow) ?? false}
+          onChange={action((e) => {
+            const c = e.currentTarget.checked;
+            const f = props.store.getFilter('time');
+
+            if (c) f.add(isTomorrow);
+            if (!c) f.remove(isTomorrow);
+          })}
+        />
+      </label>
+      <label>
+        this week
+        <input
+          type="checkbox"
+          checked={props.store.getFilter('time')?.has(isThisWeek) ?? false}
+          onChange={action((e) => {
+            const c = e.currentTarget.checked;
+            const f = props.store.getFilter('time');
+
+            if (c) f.add(isThisWeek);
+            if (!c) f.remove(isThisWeek);
+          })}
+        />
+      </label>
+      <label>
+        this month
+        <input
+          type="checkbox"
+          checked={props.store.getFilter('time')?.has(isThisMonth) ?? false}
+          onChange={action((e) => {
+            const c = e.currentTarget.checked;
+            const f = props.store.getFilter('time');
+
+            if (c) f.add(isThisMonth);
+            if (!c) f.remove(isThisMonth);
+          })}
+        />
+      </label>
+      <label>
+        next month
+        <input
+          type="checkbox"
+          checked={props.store.getFilter('time')?.has(isNextMonth) ?? false}
+          onChange={action((e) => {
+            const c = e.currentTarget.checked;
+            const f = props.store.getFilter('time');
+
+            if (c) f.add(isNextMonth);
+            if (!c) f.remove(isNextMonth);
+          })}
+        />
+      </label>
       <br />
       <button onClick={action(() => props.store.resetFilters())}>
         Remove all filters
--- src/Filters.tsx
+++ src/Filters.tsx
@@ -1,15 +1,17 @@
-import { includes, isArray, uniq } from 'lodash';
-import { makeAutoObservable } from 'mobx';
+import { includes, uniq } from 'lodash';
+import { makeAutoObservable, toJS } from 'mobx';
 import { Item } from './Item';
 
-export type FilterType = 'language' | 'level';
 type ApplyType = (item: Item) => boolean;
 
+export type FilterType = 'language' | 'level' | 'time';
 
 export type Filter<T> = T extends 'language'
   ? FilterByVal
   : T extends 'level'
   ? FilterByArr
+  : T extends 'time'
+  ? FilterByMap
   : never;
 
 export interface FilterInterface {
@@ -77,5 +79,84 @@ export class FilterByArr implements FilterInterface {
 
   removeValue(val: string): void {
     this.value = this.value.filter((i) => i !== val);
+  }
+}
+
+interface FilterCollectionInterface extends FilterInterface {
+  add(filter: FilterInterface): void;
+  remove(filter: FilterInterface): boolean;
+  has(filter: FilterInterface): boolean;
+}
+
+// ugly but I don't know how to make it better...
+declare global {
+  interface Map<K, V> {
+    toArray(): Array<FilterInterface>;
+  }
+}
+
+export class FilterByMap implements FilterCollectionInterface {
+  public key: string;
+  protected value: Map<string, FilterInterface>;
+  public apply: ApplyType;
+
+  constructor({
+    key,
+    value,
+    apply,
+  }: {
+    key: string;
+    value?: Map<string, FilterInterface>;
+    apply: ApplyType;
+  }) {
+    makeAutoObservable(this);
+    this.key = key;
+    this.value = value || new Map();
+    this.apply = apply;
+
+    this.updatePrototype();
+  }
+
+  protected updatePrototype(): void {
+    // easier conversion to array
+    this.value.toArray = function (): Array<FilterInterface> {
+      const arr: Array<FilterInterface> = [];
+      this.forEach((filter) => arr.push(filter));
+      return arr;
+    };
+
+    // return native JS object when using .forEach()
+    this.value.forEach = function (
+      callback: (
+        value: FilterInterface,
+        key: string,
+        map: Map<any, FilterInterface>
+      ) => void,
+      thisArg?: any
+    ): void {
+      if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
+      }
+      thisArg = thisArg || this;
+      for (const [k, v] of this.entries()) {
+        callback.call(thisArg, toJS(v), k, this);
+      }
+    };
+  }
+
+  add(filter: FilterInterface): void {
+    this.value.set(filter.key, filter);
+  }
+
+  remove(filter: FilterInterface): boolean {
+    return this.value.delete(filter.key);
+  }
+
+  has(filter: FilterInterface): boolean {
+    return this.value.get(filter.key) !== undefined;
+  }
+
+  reset(): void {
+    this.value.clear();
   }
 }
