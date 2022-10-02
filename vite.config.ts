feccc42176d82912769fc3290fbc6746935b5424
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2022-10-02 03:58:05 +0300

    Add library mode to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 /// <reference path="environment.d.ts" />
 
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 import { loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
@@ -13,6 +13,21 @@ export default ({ mode }) => {
     server: {
       host: process.env.VITE_HOST ?? 'localhost',
       port: process.env.VITE_PORT ? parseInt(process.env.VITE_PORT) : 5173,
+    },
+    // https://vitejs.dev/guide/build.html#library-mode
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'src/useFilters'),
+        name: 'CoolFilters',
+      },
+      rollupOptions: {
+        external: ['react'],
+        output: {
+          globals: {
+            react: 'React',
+          },
+        },
+      },
     },
   };
 };
